ac5b89e02e8f295bb8cd6ae3970aa2fa288ec68c
kauanbatista57 <kauanbsantos57@gmail.com>
2025-11-08 13:58:50 -0300

    add: routes

--- livros/src/routes.js
+++ livros/src/routes.js
@@ -1,7 +1,7 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 import Home from "./pages/Home/home";
-import Login from "./pages/Login/Login";
+import Login from "./pages/Login/login";
 
 function RoutesApp() {
   return (
